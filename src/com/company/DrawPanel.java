2bf49ffdff6a3f3d76ed7b223732442a328553bc
katya <katyamit5@gmail.com>
2022-09-15 14:20:09 +0300

    анимация облаков

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -6,9 +6,37 @@ import com.company.elements.Tree;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel{
+
+    private final Cloud cloud1;
+    private final Cloud cloud2;
+    private final Cloud cloud3;
+    private final Cloud cloud4;
+    private final Cloud cloud5;
+
     public DrawPanel() {
+        cloud1 = new Cloud(0, 0, 9, 60, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
+        cloud2 = new Cloud(500, 10, 7, 70, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
+        cloud3 = new Cloud(200, 70, 10, 80, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
+        cloud4 = new Cloud(300, 150, 5, 50, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
+        cloud5 = new Cloud(400, 250, 7, 60, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
+
+        ActionListener timerListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cloud1.setX(cloud1.getX() + 1);
+                cloud2.setX(cloud2.getX() + 1);
+                cloud3.setX(cloud3.getX() + 1);
+                cloud4.setX(cloud3.getX() + 1);
+                cloud5.setX(cloud3.getX() + 1);
+                repaint();
+            }
+        };
+        Timer timer = new Timer(10, timerListener);
+        timer.start();
+
     }
 
     @Override
@@ -22,42 +50,30 @@ public class DrawPanel extends JPanel {
         g.fillRect(0,0, 1600, 400);
 
         g.setColor(new Color(0, 255, 127));
-////        g.drawLine(10, 10, 200, 100);
-////        g.drawRect(50, 10, 30, 100);
-////        g.drawOval(50, 10, 30, 100);
-////        g.drawArc(50, 10, 200, 100, 30, 90);
-//        g.drawString("some text", 50, 10);
-////        final int SIZE = 1000;
-////        int[] x = new int[SIZE];
-////        int[] y = new int[SIZE];
-////        for(int i = 0; i < SIZE; i++){
-////            x[i] = i;
-////            y[i] = 100 + (int)(30*Math.sin(i*0.1));
-////        }
-////        g.drawPolyline(x, y, SIZE);
-//        Polygon s = new Polygon();
-//        for(int i = 0; i < 50; i++){
-//            s.addPoint(i + 50, 100 + (int)(30*Math.sin(i*0.1)));
-//        }
-//        g.draw(s);
 
-
-        Cloud cloud1 = new Cloud(90, 70, 9, 80, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
-        Cloud cloud2 = new Cloud(900, 140, 7, 80, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
-        Cloud cloud3 = new Cloud(400, 40, 13, 80, new Color(255, 240, 245), new Color(245, 255, 250), new Color(240, 248, 255));
         cloud1.draw(g);
         cloud2.draw(g);
         cloud3.draw(g);
+        cloud4.draw(g);
+        cloud5.draw(g);
 
-        Tree tree1 = new Tree(200, 600, 85, new Color(160, 82, 45), new Color(34, 139, 34));
-        Tree tree2 = new Tree(900, 600, 85, new Color(160, 82, 45), new Color(34, 139, 34));
+
+
+        Tree tree1 = new Tree(50, 650, 85, new Color(160, 82, 45), new Color(34, 139, 34));
+        Tree tree2 = new Tree(250, 600, 85, new Color(139, 69, 19), new Color(0, 128, 0));
+        Tree tree3 = new Tree(450, 550, 85, new Color(205, 133, 63), new Color(0, 100, 0));
+        Tree tree4 = new Tree(700, 550, 85, new Color(205, 133, 63), new Color(0, 100, 0));
+        Tree tree5 = new Tree(900, 600, 85, new Color(139, 69, 19), new Color(0, 128, 0));
+        Tree tree6 = new Tree(1100, 650, 85, new Color(160, 82, 45), new Color(34, 139, 34));
 
         tree1.draw(g);
         tree2.draw(g);
+        tree3.draw(g);
+        tree4.draw(g);
+        tree5.draw(g);
+        tree6.draw(g);
 
-        Tractor tractor1 =  new Tractor(400, 800, new Color(255, 140, 0));
+        Tractor tractor1 =  new Tractor(400, 850, new Color(255, 140, 0));
         tractor1.draw(g);
     }
-
-
 }
--- src/com/company/elements/Cloud.java
+++ src/com/company/elements/Cloud.java
@@ -5,8 +5,11 @@ import com.company.DrawingUtils;
 import java.awt.*;
 
 public class Cloud {
-    private final int x, y, n, diam;
-    private final Color c1, c2, c3;
+    private int x, y, n, diam;
+    private final Color c1;
+    private final Color c2;
+    private final Color c3;
+
 
     public Cloud(int x, int y, int n, int diam, Color c1, Color c2, Color c3) {
         this.x = x;
@@ -16,9 +19,54 @@ public class Cloud {
         this.c1 = c1;
         this.c2 = c2;
         this.c3 = c3;
+
     }
 
     public void draw(Graphics2D g){
         DrawingUtils.drawCloude(g, x, y, n, diam, c1, c2, c3);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getDiam() {
+        return diam;
+    }
+
+    public void setDiam(int diam) {
+        this.diam = diam;
+    }
+
+    public Color getC1() {
+        return c1;
+    }
+
+    public Color getC2() {
+        return c2;
+    }
+
+    public Color getC3() {
+        return c3;
     }
 }
